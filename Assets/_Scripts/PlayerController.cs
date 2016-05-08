871a9a707c75fcaa867b7504661b10176cc00b1a
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-05-08 13:22:42 +0700

    Prevent player from controlling dead avatar.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,6 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	float inputX;
+	float inputY;
+	SpriteRenderer spriteRenderer;
+	Animator anim;
+	Health health;
+
+
+	void Awake() {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		anim = GetComponent<Animator>();
+		health = GetComponent<Health>();
+	}
+
+	void Update() {
+		inputX = Input.GetAxisRaw("Horizontal");
+		inputY = Input.GetAxisRaw("Vertical");
+		
+		if (health.Current > 0) {
+			anim.SetFloat("InputX", inputX);
+			anim.SetFloat("InputY", inputY);
+			anim.SetBool("IsWalking", inputX != 0.0f || inputY != 0.0f);
+			
+			spriteRenderer.flipX = (inputX > 0.0f) ? false : (inputX < 0.0f) ? true : spriteRenderer.flipX;
+		}
+	}
 }
--- Assets/_Scripts/PlayerMotor.cs
+++ Assets/_Scripts/PlayerMotor.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class PlayerMotor : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	[SerializeField]
+	float moveSpeed;
+
+
+	float inputX;
+	float inputY;
+
+	Vector2 moveVector;
+	Rigidbody2D rigid;
+	Health health;
+	Stamina stamina;
+
+
+	public PlayerMotor() {
+		moveSpeed = 7.0f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Awake() {
+		rigid = GetComponent<Rigidbody2D>();
+		health = GetComponent<Health>();
+		stamina = GetComponent<Stamina>();
+	}
+
+	void Update() {
+		inputX = Input.GetAxisRaw("Horizontal");
+		inputY = Input.GetAxisRaw("Vertical");
+		
+		moveVector = new Vector2(inputX, inputY) * moveSpeed;
+		moveVector = (moveVector.magnitude > 1) ? new Vector2(moveVector.normalized.x, moveVector.normalized.y) * moveSpeed : moveVector;
+	}
+
+	void FixedUpdate() {
+		rigid.velocity = (health.Current > 0) ? moveVector : Vector2.zero;
 	}
 }
