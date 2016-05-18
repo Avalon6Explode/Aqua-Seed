92f093323b102361e128604c5d22127052b9302a
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-05-19 05:05:07 +0700

    Added dynamic sprite layer changer to make player render  when player's y pos > monster's y pos (render monster on top) player's y pos < monster's y pos (render player on top)

---
+++ Assets/_Scripts/Enemy/EnemySortingLayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemySortingLayerController : MonoBehaviour {
+
+	const string BEHIND_LAYER = "Humaniod";
+	const string INFRONT_LAYER = "Enemy";
+
+
+	float currentRefPosY;
+	SpriteRenderer spriteRenderer;
+
+
+	void Awake() {
+		spriteRenderer = transform.parent.gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void Update() {
+		if (currentRefPosY > transform.position.y) {
+			spriteRenderer.sortingLayerName = INFRONT_LAYER;
+		}
+		else if (currentRefPosY < transform.position.y) {
+			spriteRenderer.sortingLayerName = BEHIND_LAYER;;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			currentRefPosY = col.gameObject.transform.position.y;
+		}
+	}
+}
