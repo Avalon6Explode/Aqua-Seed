c93ccf0e22fe347514cb3730281a945b57d93574
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-05-21 07:48:06 +0700

    Add Story UI

---
+++ Assets/_Scripts/UI/Controller/UIStoryController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStoryController : MonoBehaviour {
+
+	int currentChooseImageIndex;
+
+	[SerializeField]
+	Image imageHolder;
+
+	[SerializeField]
+	Text textHolder;
+
+	[SerializeField]
+	Sprite[] images;
+
+	[SerializeField]
+	string[] texts;
+
+
+	void Start() {
+
+		textHolder.text = texts[currentChooseImageIndex];
+		imageHolder.sprite = images[currentChooseImageIndex];
+
+	}
+
+
+
+	public void NextImage() {
+
+		var tempNum = currentChooseImageIndex + 1;
+
+		if (tempNum > images.Length - 1) {
+
+			tempNum = 0;
+
+		}
+
+		currentChooseImageIndex = tempNum;
+		textHolder.text = texts[currentChooseImageIndex];
+		imageHolder.sprite = images[currentChooseImageIndex];
+
+	}
+
+
+	public void PreviousImage() {
+
+		var tempNum = currentChooseImageIndex - 1;
+
+		if (tempNum < 0) {
+
+			tempNum = images.Length - 1;
+
+		}
+
+		currentChooseImageIndex = tempNum;
+		textHolder.text = texts[currentChooseImageIndex];
+		imageHolder.sprite = images[currentChooseImageIndex];
+
+	}
+}
